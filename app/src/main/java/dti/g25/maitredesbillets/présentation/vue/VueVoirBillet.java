5aa5e0668ee0b3c8a1b26db9e2cffe5021bc80d1
Guillaume <guillaume@chief>
2020-03-16 02:14:37 -0400

    ok

--- app/src/main/java/dti/g25/maitredesbillets/présentation/vue/VueVoirBillet.java
+++ app/src/main/java/dti/g25/maitredesbillets/présentation/vue/VueVoirBillet.java
@@ -61,53 +61,53 @@ public class VueVoirBillet extends Fragment implements ContratVuePrésenteurVoir
     }
 
     //cree le swipe gauche pour le rv
-        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0,ItemTouchHelper.RIGHT) {
+    final ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
 
-            /**
-             * Cette méthode n'Est pa utilisée pour l'intsant
-             * @param recyclerView le rv des projets
-             * @param viewHolder
-             * @param target
-             * @return null
-             */
-            @Override
-            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-                return false;
-            }
+        /**
+         * Cette méthode n'Est pa utilisée pour l'intsant
+         * @param recyclerView le rv des projets
+         * @param viewHolder
+         * @param target
+         * @return null
+         */
+        @Override
+        public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+            return false;
+        }
 
-            /**
-             * Ce qu'il se passe quand on swipe un element du rv selon une direction
-             * @param viewHolder le viewHolder
-             * @param direction la dr
-             */
-            @Override
-            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-                int position=viewHolder.getAdapterPosition();
-                présenteur.requêteSupprimerBillet(position);
-            }
+        /**
+         * Ce qu'il se passe quand on swipe un element du rv selon une direction
+         * @param viewHolder le viewHolder
+         * @param direction la dr
+         */
+        @Override
+        public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+            int position = viewHolder.getAdapterPosition();
+            présenteur.requêteSupprimerBillet(position);
+        }
 
-            /**
-             * Décore le swipe vers la droite, y met la couleure ansi que l'icone
-             * J'ai pris cette méthode d'une librairie sur git, voir le fichier gradle.build de l'app
-             * @param c
-             * @param recyclerView
-             * @param viewHolder
-             * @param dX
-             * @param dY
-             * @param actionState
-             * @param isCurrentlyActive
-             */
-            @Override
-            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
-                new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-                        .addBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorAccent))
-                        .addActionIcon(R.drawable.ic_remove_circle_black_24dp)
-                        .create()
-                        .decorate();
+        /**
+         * Décore le swipe vers la droite, y met la couleure ansi que l'icone
+         * J'ai pris cette méthode d'une librairie sur git, voir le fichier gradle.build de l'app
+         * @param c
+         * @param recyclerView
+         * @param viewHolder
+         * @param dX
+         * @param dY
+         * @param actionState
+         * @param isCurrentlyActive
+         */
+        @Override
+        public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+            new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                    .addBackgroundColor(ContextCompat.getColor(getContext(), R.color.colorAccent))
+                    .addActionIcon(R.drawable.ic_remove_circle_black_24dp)
+                    .create()
+                    .decorate();
 
-                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
-            }
-        };
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
+    };
 
 
 }
