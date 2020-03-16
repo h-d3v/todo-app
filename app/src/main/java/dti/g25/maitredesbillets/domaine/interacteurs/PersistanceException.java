6eee7ec84ea4d268527de6d8c8686f9f00834489
Julien Jacquard <jacquardjul@gmail.com>
2020-03-16 12:46:38 -0400

    Nettoyage de fichiers non utilises

--- app/src/main/java/dti/g25/maitredesbillets/domaine/interacteurs/PersistanceException.java
+++
@@ -1,9 +0,0 @@
-package dti.g25.maitredesbillets.domaine.interacteurs;
-
-public class PersistanceException extends Exception {
-    public Exception exception;
-
-    public PersistanceException(Exception e) {
-        exception=e;
-    }
-}
